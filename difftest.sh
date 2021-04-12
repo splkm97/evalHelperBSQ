411b1f92ac2a78b35712692de58cc4ac3802b242
= <=>
2021-04-12 21:26:54 +0900

    add difftest.sh

---
+++ difftest.sh
@@ -0,0 +1,19 @@
+den=1
+wid=10
+hei=10
+
+while [ ${den} -le 3 ]
+do
+	hei=10
+	while [ ${hei} -le 30 ]
+	do
+		wid=10
+		while [ ${wid} -le 40 ]
+		do
+			diff "examples/ex${wid}${hei}${den}.map" "examples/ex${wid}${hei}${den}.ans" > "diffs/ex${wid}${hei}${den}.diff"
+			wid=`expr ${wid} \+ 10`
+		done
+		hei=`expr ${hei} \+ 10`
+	done
+	((den++))
+done
